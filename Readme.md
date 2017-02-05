1ed26d4e307c5178b70acc01521568888e4bfcd9
Artem Sapegin <artem@sapegin.ru>
2017-02-05 18:20:01 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# proselint
+
+[Proselint](http://proselint.com/) wrapper with a friendly reporter. Also skips text in Markdown code blocks.
+
+![](https://d3vv6lp55qjaqc.cloudfront.net/items/3i013t3b443t0P0Z3c1o/proselint.png)
+
+## Installation
+
+```bash
+pip install proselint
+npm install -g proselint
+```
+
+## Usage
+
+```bash
+proselintjs <filename.md>
+```
+
+## Changelog
+
+The changelog can be found on the [Releases page](https://github.com/sapegin/proselint/releases).
+
+## Contributing
+
+Everyone is welcome to contribute. Please take a moment to review the [contributing guidelines](Contributing.md).
+
+## Authors and license
+
+[Artem Sapegin](http://sapegin.me) and [contributors](https://github.com/sapegin/proselint/graphs/contributors).
+
+MIT License, see the included [License.md](License.md) file.
---
+++ index.js
@@ -0,0 +1,134 @@
+#!/usr/bin/env node
+
+
+/* eslint-disable no-console */
+
+const fs = require('fs');
+const exec = require('child_process').exec;
+const tempWrite = require('temp-write');
+const splitLines = require('split-lines');
+const chalk = require('chalk');
+const remark = require('remark');
+const visit = require('unist-util-visit');
+const codeFrame = require('babel-code-frame');
+
+const NOT_FOUND = 127;
+const ICONS = {
+	warning: '⚠',
+	error: '✘',
+};
+const COLORS = {
+	warning: 'yellow',
+	error: 'red',
+};
+
+/* ---------------- 8< -------- 8< ---------------- */
+
+const filepath = process.argv[2];
+if (!filepath) {
+	usage();
+	process.exit(1);
+}
+
+run(filepath);
+
+/* ---------------- 8< -------- 8< ---------------- */
+
+function run(filepath) {
+	if (!fs.existsSync(filepath)) {
+		fileNotFound(filepath);
+		process.exit(1);
+	}
+
+	const contents = fs.readFileSync(filepath, 'utf8');
+	const safeContents = replaceCodeBlocks(contents);
+	const tempFilepath = tempWrite.sync(safeContents, filepath);
+
+	exec(`proselint --json ${tempFilepath}`, (error, stdout) => {
+		if (error) {
+			if (error.code === NOT_FOUND) {
+				binaryNotFound();
+				process.exit(1);
+			}
+
+			const json = JSON.parse(stdout);
+			printErrors(contents, json.data.errors);
+		}
+		else {
+			console.log(chalk.green('No issues found.'));
+		}
+	});
+}
+
+// Replace all lines inside code blocks with empty lines becase proselint validates code by default.
+// We use empty lines instead of just code removal to keep correct line numbers.
+// Work with the original document because Remark changes formatting which makes error positions incorrect
+function replaceCodeBlocks(contents) {
+	function processCode() {
+		return ast => {
+			visit(ast, 'code', node => {
+				const start = node.position.start.line;
+				const end = node.position.end.line;
+				for (let line = start; line < end - 1; line++) {
+					lines[line] = '';
+				}
+			});
+		};
+	}
+
+	const lines = splitLines(contents);
+	remark()
+		.use(processCode)
+		.process(contents)
+	;
+	return lines.join('\n');
+}
+
+function printErrors(contents, errors) {
+	errors.forEach(error => printError(contents, error));
+}
+
+function printError(contents, { line, column, severity, message, check, replacements }) {
+	const code = codeFrame(contents, line, column);
+	console.log([
+		'',
+		chalk.bold[COLORS[severity]](ICONS[severity] + ' ' + message) + ' ' + chalk.dim(check),
+		replacements && ('Suggestions: ' + replacements),
+		'',
+		code,
+		'',
+	].join('\n'));
+}
+
+function usage() {
+	console.log([
+		chalk.underline('Usage'),
+		'',
+		'    ' + chalk.bold('proselintjs') + ' ' + chalk.cyan('<filename.md>'),
+		'',
+	].join('\n'));
+}
+
+function fileNotFound(filepath) {
+	console.log([
+		chalk.bold.red('File not found'),
+		'',
+		'    ' + filepath,
+		'',
+	].join('\n'));
+}
+
+function binaryNotFound() {
+	console.log([
+		chalk.bold.red('Proselint not found'),
+		'',
+		'Install proselint first:',
+		'',
+		'    pip install proselint',
+		'',
+		'More information:',
+		'',
+		'    ' + chalk.underline('https://github.com/amperser/proselint/'),
+		'',
+	].join('\n'));
+}
